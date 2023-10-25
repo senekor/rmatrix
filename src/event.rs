312f36dba3af8e77f845a3c2cf77916f7214a03e
Remo Senekowitsch <remo@buenzli.dev>
2023-10-25 22:42:48 +0200

    wip improve event handling

--- src/event.rs
+++ src/event.rs
@@ -23,57 +23,55 @@ pub enum Event {
 /// Terminal event handler.
 #[derive(Debug)]
 pub struct EventHandler {
-    /// Event sender channel.
-    _sender: mpsc::Sender<Event>,
-    /// Event receiver channel.
     receiver: mpsc::Receiver<Event>,
-    /// Event handler thread.
-    _handler: thread::JoinHandle<()>,
 }
+
+// 30 fps
+static TICK_RATE: Duration = Duration::from_nanos(33333333);
+// 60 fps
+// static TICK_RATE: Duration = Duration::from_nanos(16666667);
 
 impl EventHandler {
     /// Constructs a new instance of [`EventHandler`].
     pub fn new() -> Self {
-        // 30 fps
-        let tick_rate = Duration::from_nanos(33333333);
         let (sender, receiver) = mpsc::channel();
-        let handler = {
-            let sender = sender.clone();
-            thread::spawn(move || {
-                let mut last_tick = Instant::now();
-                loop {
-                    let timeout = tick_rate
-                        .checked_sub(last_tick.elapsed())
-                        .unwrap_or(tick_rate);
 
-                    if event::poll(timeout).expect("no events available") {
-                        match event::read().expect("unable to read event") {
-                            CrosstermEvent::Key(e) => {
-                                if e.kind == event::KeyEventKind::Press {
-                                    sender.send(Event::Key(e))
-                                } else {
-                                    Ok(()) // ignore KeyEventKind::Release on windows
-                                }
+        let s = sender.clone();
+        thread::spawn(move || {
+            let sender = s;
+            let mut last_tick = Instant::now();
+            loop {
+                let timeout = TICK_RATE
+                    .checked_sub(last_tick.elapsed())
+                    .unwrap_or(TICK_RATE);
+                thread::sleep(timeout);
+
+                last_tick = Instant::now();
+                sender.send(Event::Tick).expect("failed to send tick event");
+            }
+        });
+
+        thread::spawn(move || {
+            loop {
+                if event::poll(Duration::from_millis(1000)).expect("no events available") {
+                    match event::read().expect("unable to read event") {
+                        CrosstermEvent::Key(e) => {
+                            if e.kind == event::KeyEventKind::Press {
+                                sender.send(Event::Key(e))
+                            } else {
+                                Ok(()) // ignore KeyEventKind::Release on windows
                             }
-                            CrosstermEvent::Mouse(e) => sender.send(Event::Mouse(e)),
-                            CrosstermEvent::Resize(w, h) => sender.send(Event::Resize(w, h)),
-                            _ => unimplemented!(),
                         }
-                        .expect("failed to send terminal event")
+                        CrosstermEvent::Mouse(e) => sender.send(Event::Mouse(e)),
+                        CrosstermEvent::Resize(w, h) => sender.send(Event::Resize(w, h)),
+                        _ => unimplemented!(),
                     }
-
-                    if last_tick.elapsed() >= tick_rate {
-                        sender.send(Event::Tick).expect("failed to send tick event");
-                        last_tick = Instant::now();
-                    }
+                    .expect("failed to send terminal event")
                 }
-            })
-        };
-        Self {
-            _sender: sender,
-            receiver,
-            _handler: handler,
-        }
+            }
+        });
+
+        Self { receiver }
     }
 
     /// Receive the next event from the handler thread.
